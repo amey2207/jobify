f4318e98cd28eabe1ba4ecaa61192c648d7155a8
sabeeh shah <sabeeh.shah@hotmail.com>
2020-02-08 02:26:18 -0500

    added model class for user

--- app/src/main/java/com/sheridan/jobpill/EditProfileActivity.java
+++ app/src/main/java/com/sheridan/jobpill/EditProfileActivity.java
@@ -3,12 +3,42 @@ package com.sheridan.jobpill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    private ImageView imgCancelIcon;
+    private ImageView imgSaveIcon;
+
+    private TextView txtChangeProfileImg;
+    private CircleImageView imgChangeProfile;
+
+    private EditText txtProfileName;
+    private EditText txtProfileIntro;
+    private EditText txtProfilePhone;
+    private EditText txtProfileCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+    }
+
+    public void setUpWidgets(){
+        imgCancelIcon = findViewById(R.id.img_cancel_editprofile);
+        imgSaveIcon = findViewById(R.id.img_save_editprofile);
+
+        txtChangeProfileImg = findViewById(R.id.txt_change_imgprofile);
+        imgChangeProfile = findViewById(R.id.img_change_profile);
+
+        txtProfileName = findViewById(R.id.txt_profile_name);
+        txtProfileIntro = findViewById(R.id.txt_profile_intro);
+        txtProfilePhone = findViewById(R.id.txt_profile_phone);
+        txtProfileCity = findViewById(R.id.txt_profile_city);
+
     }
 }
---
+++ app/src/main/java/com/sheridan/jobpill/Models/User.java
@@ -0,0 +1,52 @@
+package com.sheridan.jobpill.Models;
+
+public class User {
+
+    private String name;
+    private String introduction;
+    private String phone;
+    private String city;
+    private String photoURL;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+}
