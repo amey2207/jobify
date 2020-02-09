b7348d3ffcc67c8e3da9cc5b6d6874545a8046a3
Anton Kopti <Owner@DESKTOP-PTHU1UP>
2020-02-08 22:10:29 -0500

    - Login View now fully supports Firebase - changed textfield IDs - Made Login the homepage - Added firebase dependencies to the project

--- app/src/main/java/com/sheridan/jobpill/LoginActivity.java
+++ app/src/main/java/com/sheridan/jobpill/LoginActivity.java
@@ -1,16 +1,52 @@
 package com.sheridan.jobpill;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText userEmail;
+    EditText userPass;
+    Button userLogin;
+
+    FirebaseAuth fb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_login);
+
+        userEmail = findViewById(R.id.editEmail);
+        userPass = findViewById(R.id.editPass);
+        userLogin = findViewById(R.id.btn_login);
+        fb = FirebaseAuth.getInstance();
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fb.signInWithEmailAndPassword(userEmail.getText().toString(), userPass.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
 
