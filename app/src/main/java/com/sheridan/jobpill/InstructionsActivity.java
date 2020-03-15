b6e2dd7eedba03d61662046da307ef03f17501e5
Anton Kopti <Owner@DESKTOP-PTHU1UP>
2020-03-14 22:53:11 -0400

    Added job posting functionality -Fixed the UI constraint for job posting.

--- app/src/main/java/com/sheridan/jobpill/InstructionsActivity.java
+++ app/src/main/java/com/sheridan/jobpill/InstructionsActivity.java
@@ -4,13 +4,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.database.DatabaseReference;
+
+import com.sheridan.jobpill.Models.Job;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class InstructionsActivity extends AppCompatActivity {
 
     Button CancelBtn;
     Button PostBtn;
+    EditText InstructionsEdt;
+    DatabaseReference reference;
+    Job job;
+    String user_id;
+    private FirebaseFirestore db;
+    CollectionReference dbJobs;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentUser;
+    Map<String, Object> jobMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +51,34 @@ public class InstructionsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_job_posting_instructions);
         CancelBtn = findViewById(R.id.cancel_btn);
         PostBtn = findViewById(R.id.post_btn);
+        InstructionsEdt = findViewById(R.id.instructions_editText);
+        Intent intent = getIntent();
+        String jobTitle = intent.getStringExtra("Job_Title");
+        long jobPayment = Long.parseLong(intent.getStringExtra("Job_Payment"));
+        String jobLocation = intent.getStringExtra("Job_Location");
+        String jobDescription = intent.getStringExtra("Job_Description");
+        String jobCategory = intent.getStringExtra("Job_Category");
+        String jobInstructions = InstructionsEdt.getText().toString();
+        firebaseAuth = FirebaseAuth.getInstance();
+        user_id = firebaseAuth.getCurrentUser().getUid();
+        currentUser = firebaseAuth.getCurrentUser();
+
+        reference = FirebaseDatabase.getInstance().getReference().child("jobs").child(user_id);
+        String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        jobMap = new HashMap<>();
+        jobMap.put("Title", jobTitle);
+        jobMap.put("createdBy", currentUser.getEmail());
+        jobMap.put("createdDate", date);
+        jobMap.put("estimatedPay", jobPayment);
+        jobMap.put("instructions", jobInstructions);
+        jobMap.put("jobCategory", jobCategory);
+        jobMap.put("jobDescription", jobDescription);
+        jobMap.put("jobStatus", "available");
+        jobMap.put("jobTitle", jobTitle);
+        jobMap.put("location", jobLocation);
+        jobMap.put("photoURL", "test");
+        db = FirebaseFirestore.getInstance();
+        dbJobs = db.collection("jobs");
         CancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,12 +87,24 @@ public class InstructionsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         PostBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent jobIntent = new Intent(InstructionsActivity.this, MainActivity.class);
-                startActivity(jobIntent);
-                finish();
+                dbJobs.add(jobMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "Job posted successfully", Toast.LENGTH_LONG).show();
+                        Intent jobIntent = new Intent(InstructionsActivity.this, MainActivity.class);
+                        startActivity(jobIntent);
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/sheridan/jobpill/JobPostingActivity.java
+++ app/src/main/java/com/sheridan/jobpill/JobPostingActivity.java
@@ -1,30 +1,95 @@
 package com.sheridan.jobpill;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class JobPostingActivity extends AppCompatActivity {
 
     Button nextBtn;
+    Button cancelBtn;
+    ImageButton jobImageBtn;
+    EditText titleEdt;
+    EditText paymentEdt;
+    EditText locationEdt;
+    EditText descriptionEdt;
+    Spinner categorySpn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_posting);
+        cancelBtn = findViewById(R.id.cancel_post_btn);
+        jobImageBtn = findViewById(R.id.job_imagebtn);
+        titleEdt = findViewById(R.id.Job_title_editText);
+        paymentEdt = findViewById(R.id.payment_editText);
+        locationEdt = findViewById(R.id.Location_editText);
+        descriptionEdt = findViewById(R.id.description_editText);
+        categorySpn = findViewById(R.id.cat_spinner);
         nextBtn = findViewById(R.id.Next_btn);
+        jobImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ContextCompat.checkSelfPermission(JobPostingActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(JobPostingActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
+                        ActivityCompat.requestPermissions(JobPostingActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    } else {
+                        BringImagePicker();
+                    }
+
+                } else {
+                    BringImagePicker();
+                }
+            }
+        });
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent jobIntent = new Intent(JobPostingActivity.this, MainActivity.class);
+                startActivity(jobIntent);
+                finish();
+            }
+        });
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String jobTitle = titleEdt.getText().toString();
+                String jobPayment = paymentEdt.getText().toString();
+                String jobLocation = locationEdt.getText().toString();
+                String jobDescription = descriptionEdt.getText().toString();
+                String jobCategory = categorySpn.getSelectedItem().toString();
+
                 Intent jobIntent = new Intent(JobPostingActivity.this, InstructionsActivity.class);
+                jobIntent.putExtra("Job_Title", jobTitle);
+                jobIntent.putExtra("Job_Payment", jobPayment);
+                jobIntent.putExtra("Job_Location", jobLocation);
+                jobIntent.putExtra("Job_Description", jobDescription);
+                jobIntent.putExtra("Job_Category", jobCategory);
                 startActivity(jobIntent);
                 finish();
             }
@@ -35,16 +100,28 @@ public class JobPostingActivity extends AppCompatActivity {
     public void Instructions_page(View view) {
         startActivity(new Intent(this, InstructionsActivity.class));
     }
+
     public void addItemsOnSpinner() {
 
         Spinner spinner2 = findViewById(R.id.cat_spinner);
         List<String> list = new ArrayList<>();
-        list.add("list 1");
-        list.add("list 2");
-        list.add("list 3");
+        list.add("Paint");
+        list.add("DIY");
+        list.add("Cleaning");
+        list.add("Gardening");
+        list.add("Babysitting");
+        list.add("Grocery Pick-up");
+        list.add("Other");
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(dataAdapter);
+    }
+
+    private void BringImagePicker() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1, 1)
+                .start(JobPostingActivity.this);
     }
 }
